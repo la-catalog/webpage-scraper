29527aaff8ecd5a4becc05ab87ee431b22f91069
thiagola92 <thiagola92@users.noreply.github.com>
2022-09-17 14:58:23 -0300

    Workflow fix

--- pages/09_🔍_sku.py
+++ pages/09_🔍_sku.py
@@ -10,18 +10,11 @@ from page_infra.options import get_marketplace_infra
 from page_models import SKU
 from pymongo import MongoClient
 from structlog.stdlib import get_logger
-from webpage_components import (
-    display_attributes,
-    display_basic,
-    display_images,
-    display_measurements,
-    display_prices,
-    display_rating,
-    display_segments,
-    display_weight,
-    search_bar,
-    search_info_bar,
-)
+from webpage_components import (display_attributes, display_basic,
+                                display_images, display_measurements,
+                                display_prices, display_rating,
+                                display_segments, display_weight, search_bar,
+                                search_info_bar)
 
 from utility.marketplaces import MARKETPLACES
 
