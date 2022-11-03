ba216604f393d4746c132066777306d2ed32a0bf
thiagola92 <thiagola92@gmail.com>
2022-11-03 01:40:04 -0300

    Update pyproject.toml

--- pages/00_🇦_search_page.py
+++ pages/00_🇦_search_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from page_infra.options import get_marketplace_infra
-from rabbit_models.search_scraper import Body
+from rabbit_models.search_scraper import Body, Metadata
 from structlog.stdlib import get_logger
 
 from utility.marketplaces import MARKETPLACES
@@ -20,7 +20,9 @@ infra = get_marketplace_infra(marketplace=marketplace, logger=get_logger())
 
 if st.button("Inserir na fila"):
     with st.spinner(text="Inserindo..."):
-        body = Body(url=url, marketplace=marketplace)
+        body = Body(
+            url=url, marketplace=marketplace, metadata=Metadata(source="webpage")
+        )
         body.metadata.source = "scraper-webpage"
         message = body.json(exclude_none=True)
 
--- pages/01_🔗_search_page.py
+++ pages/01_🔗_search_page.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from page_infra.options import get_marketplace_infra
-from rabbit_models.search_scraper import Body
+from rabbit_models.search_scraper import Body, Metadata
 from structlog.stdlib import get_logger
 
 from utility.marketplaces import MARKETPLACES, get_marketplace_index
@@ -38,9 +38,13 @@ infra = get_marketplace_infra(
 
 if st.button("Inserir na fila"):
     with st.spinner(text="Inserindo..."):
-        body = Body(url=url, marketplace=marketplace)
-        body.metadata.source = "scraper-webpage"
-        message = body.json(exclude_none=True)
+        message = Body(
+            url=url,
+            marketplace=marketplace,
+            metadata=Metadata(
+                source="webpage",
+            ),
+        ).json(exclude_none=True)
 
         publish_on_queue(message=message, queue=infra.search_queue)
 
